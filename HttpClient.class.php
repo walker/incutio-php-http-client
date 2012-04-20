7064aedea364af59f272d24289177c386de794db
Walker Hamilton <myself@walkerhamilton.com>
2012-04-20 17:37:50 -0500

    Updated. Fixed max_redirects not restarting count on ALL doRequests

--- HttpClient.class.php
+++ HttpClient.class.php
@@ -1,14 +1,13 @@
 <?php
 
 /* Version 0.9, 6th April 2003 - Simon Willison ( http://simon.incutio.com/ )
-   Further Information: https://github.com/walker/Incutio-PHP-HTTP-Client
+   Further Information: http://code.google.com/p/php-httpclient/
    Historical Details: http://scripts.incutio.com/httpclient/
    Incutio Ltd - www.incutio.com
    
 * Version 0.9d, 4th, April 2012 Walker Hamilton (http://walkerhamilton.com)
  - Added PUT & DELETE request types, reused postdata
  - Added in proper header setting for some of the items that were being set automatically or were only being set on POST
- - Created GitHub Repo: https://github.com/walker/Incutio-PHP-HTTP-Client
 */
 
 class HttpClient {
@@ -631,6 +630,7 @@ class HttpClient {
 			}
 			
 		}
+		$this->redirect_count = 0;
 		
 		return true;
 	}
