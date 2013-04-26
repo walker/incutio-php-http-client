142ae293c8cdbeb62d362ec0787292c059cfba64
Walker Hamilton <myself@walkerhamilton.com>
2013-04-26 10:52:58 -0500

    Reset default accept-language header

--- HttpClient.class.php
+++ HttpClient.class.php
@@ -36,7 +36,7 @@ class HttpClient {
     protected $referer;
     protected $accept = 'text/xml,application/xml,application/xhtml+xml,text/html,text/plain,image/png,image/jpeg,image/gif,*/*';
     protected $accept_encoding = 'gzip';
-    protected $accept_language = 'en-US,en;q=0.8'; // 'en-us';
+    protected $accept_language = 'en-us';
     protected $user_agent = 'Incutio HttpClient v0.9d';
     protected $request_headers = array();
 
